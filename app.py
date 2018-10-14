c5afe5b2d1a87a3f2158588f1ac1e9dac84ac35f
kirancr9 <subs.kiran@gmail.com>
2018-10-14 14:15:30 +0530

    ax

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/services")
+def serv():
+    return render_template("services.html")
+
+@app.route("/register")
+def reg():
+    return render_template("reg.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
